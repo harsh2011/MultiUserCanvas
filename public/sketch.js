25239b97378c66fe4a6e76d76281be6f39992346
Harsh Patel <harsh20111997@gmail.com>
2017-12-08 03:03:03 +0530

    new one

--- public/sketch.js
+++ public/sketch.js
@@ -1,9 +1,14 @@
 var socket;
 
-var incanvasid;
+var nameval;
 
 var r,g,b;
 
+var connectlist = {};
+
+var div;
+
+//generate integer between given
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -19,30 +24,57 @@ function setup() {
 	g = getRandomInt( 55, 155);
 
 	id = currentLocation.replace( 'https://multiusercanvas.herokuapp.com/canvas/', "" );
+	id = currentLocation.replace( 'http://localhost:3000/canvas/', "" );
 
 	canvasid = id;
 
 	console.log(canvasid)
 
-	socket = io.connect('https://multiusercanvas.herokuapp.com/'+canvasid);
-	
+	socket = io.connect('http://localhost:3000/'+canvasid);
+
+ 	div = document.getElementById("textDiv");  
+
 	createCanvas(1200,500);
 
 	background(51);
 
+	//get other user data
 	socket.on('mouse',newDrawing);
+	socket.on('newuser',newUser);
+	socket.on('disuser',disUser);
+	socket.on('foruser',forUser);
+
+	var person = prompt("Please enter your name", "Harry Potter");
+
+	if(person!=null){
+		nameval = person;
+		sendForConnection();
+	}
 }
 
 
-function newDrawing(data){
-	//console.log("rec:"+data);
-	
+
+function newUser(data){
+	connectlist[data.socketid] = data.name;
+	console.log(connectlist);
+	div.textContent = JSON.stringify(connectlist);
+	sendConnection();
+} 
+function forUser(data){
+	connectlist[data.socketid] = data.name;
+	div.textContent = JSON.stringify(connectlist);
+	console.log(connectlist);
+} 
+
+function disUser(data){
+	delete connectlist[data];
+	div.textContent = JSON.stringify(connectlist);
+	console.log(connectlist);
+}
+
+function newDrawing(data){	
 	fill(data.r,data.b,data.g);	
 	ellipse(data.x, data.y, 10,10);
-
-	//console.log("receiving : "+data.x +","+data.y);
-		
-	
 }
 
 function draw() {
@@ -51,12 +83,13 @@ function draw() {
 		fill(r,b,g);	
 		ellipse(mouseX, mouseY, 10,10);
 
-		//console.log("sending : "+mouseX +","+mouseY);
 		if(mouseX>0 && mouseX<1200 && mouseY>0 && mouseY<500 ){
 			sendData(mouseX,mouseY,r,b,g);
 		}
 	}
 }
+
+
 
 function sendData(mouseX,mouseY,red,blue,green){
 	var data = {
@@ -68,4 +101,19 @@ function sendData(mouseX,mouseY,red,blue,green){
 		socketid : socket.id
 	}
 	socket.emit('mouse', data);
+}
+
+function sendForConnection(){
+
+	var data = {
+		name : nameval
+	};
+	socket.emit('newuser',data);
+}
+function sendConnection(){
+
+	var data = {
+		name : nameval
+	};
+	socket.emit('foruser',data);
 }
--- server.js
+++ server.js
@@ -2,77 +2,99 @@
 
 //express library
 var express =  require('express');
+//socket library
 var socket = require('socket.io');
+//arraylist as data structure
 var ArrayList = require('arraylist');
+//Dictionary
+var dict = require("dict");
 
+
+//express app
 var app = express();
 
 var list = new ArrayList;
-var clist = new ArrayList;
-
 
 var server = app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
-app.use(express.static('public'));
+//socket intialization
+var io = socket(server);
 
 // WebSocket Portion
 // WebSockets work with the HTTP server
-var io = socket(server);
 
 var canvas;
 
+//inital page (send the public folder to client which load index.html)
+app.use(express.static('public'));
+
+//canvas page (it loads canvas.html)
 app.get('/canvas/:data', function(req, res) {
-	console.log("i am here");
+	
+	var temp = {};
+	temp["a"] = "a";
+	temp["b"] = "b";
+	console.log(temp);
+
 
 	var canvas_id =  req.params.data;
 
+	//check if the canvas id exist
 	if(list.contains(canvas_id)){
-		//nothing 
+		//when socket exist 
 		console.log("socket not created");
 	}
 	else{
+		//when socket not exist (new socket is created)
 		console.log("socket created");
-		list.add(canvas_id)
+		
+		list.add(canvas_id);
 
-		console.log(canvas_id);
-
+		//creating socket of the given ID
 		canvas = io.of(canvas_id).on('connection',newconnect); 
-
-		clist.add(canvas)
 	}
 
+	//return canvas.html
 	res.sendFile(__dirname + '/public/canvas.html');
 
 });
 
 
 function newconnect(socket){
-	console.log("We have a new client: " + socket.id);
 
-	console.log(clist[0].connected);
+	socket.on('newuser',function(data){
+		newdata= {
+			name : data.name,
+			socketid : socket.id
+		};
+		socket.broadcast.emit('newuser',newdata);
+	});
+
+	socket.on('foruser',function(data){
+		newdata= {
+			name : data.name,
+			socketid : socket.id
+		};
+		socket.broadcast.emit('foruser',newdata);
+	});
 
 	// When this user emits, client side: socket.emit('otherevent',some data);
 	socket.on('mouse',function(data) {
-		// Data comes in as whatever was sent, including objects
-		//console.log("Received: 'mouse' " + data.x + " " + data.y);
+		// Data comes for a client
 
 		// Send it to all other clients
 		socket.broadcast.emit('mouse', data);
-
-		// This is a way to send to everyone including sender
-		// io.sockets.emit('message', "this goes to everyone");
-
 	});
 
 	socket.on('disconnect', function(data){
-		console.log("disconnect " + data);
-	    console.log("Client has disconnected");
+		console.log("disconnect " + socket.id);
+		socket.broadcast.emit('disuser',socket.id);
 	});
 
 }
 
-console.log("Server running on port 3001");
+console.log("Server running on port 3000");
 
 
